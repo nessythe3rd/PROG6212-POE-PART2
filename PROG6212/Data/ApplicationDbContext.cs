da8ccd4380206c2cf22ea6a90606331d129e9088
ganee <ganee@DESKTOP-ITIIGGA>
2024-09-09 14:55:00 +0200

    Add project files.

---
+++ PROG6212/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PROG6212.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ PROG6212/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROG6212.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
